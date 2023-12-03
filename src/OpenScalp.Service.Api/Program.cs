5c2e29c3a225f7f4661686a018483a1545c7be53
exp10der <konstantin.writing.code@gmail.com>
2023-12-03 03:05:50 +0300

    (feature) open scalp service api

---
+++ src/OpenScalp.Service.Api/Program.cs
@@ -0,0 +1,78 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.OpenApi.Models;
+using Prometheus;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(hostingContext.Configuration)
+    .Enrich.FromLogContext());
+
+
+var services = builder.Services;
+
+services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+services.AddSwaggerGen(options =>
+{
+    options.EnableAnnotations();
+    options.CustomSchemaIds(x => x.FullName);
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "OpenScalp.Service.Api",
+        Description = "OpenScalp Service Api"
+    });
+});
+
+if (builder.Environment.IsDevelopment())
+{
+    services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+        });
+    });
+}
+
+
+services.AddHealthChecks().ForwardToPrometheus();
+
+services.AddHttpLogging(logging => { logging.LoggingFields = HttpLoggingFields.All; });
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseCors();
+app.UseHttpMetrics();
+app.UseWhen(
+    context => context.Request.Path.StartsWithSegments("/api"),
+    cfg => cfg.UseHttpLogging()
+);
+
+app.MapControllers();
+
+app.UseSwagger(options => { options.RouteTemplate = "swagger/{documentName}/swagger.json"; })
+    .UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = "swagger";
+        c.SwaggerEndpoint("v1/swagger.json", "OpenScalp api v1");
+    });
+
+app.MapHealthChecks("/health");
+app.MapMetrics();
+
+app.Run();
